a27a6e238a7941e9b2e2c345add3e57bbe37879d
itsme-alok <112114266+itsme-alok@users.noreply.github.com>
2024-07-09 00:19:42 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -1,17 +1,13 @@
-#Github.com/Vasusen-code
-
 from pyrogram import Client
-
 from telethon.sessions import StringSession
 from telethon.sync import TelegramClient
-
 from decouple import config
 import logging, time, sys
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
 
-# variables
+# Variables
 API_ID = config("API_ID", default=None, cast=int)
 API_HASH = config("API_HASH", default=None)
 BOT_TOKEN = config("BOT_TOKEN", default=None)
@@ -19,22 +15,25 @@ SESSION = config("SESSION", default=None)
 FORCESUB = config("FORCESUB", default=None)
 AUTH = config("AUTH", default=None, cast=int)
 
-bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
+if API_ID is None or API_HASH is None or BOT_TOKEN is None:
+    raise ValueError("API_ID, API_HASH, and BOT_TOKEN must be set")
 
-userbot = Client("saverestricted", session_string=SESSION, api_hash=API_HASH, api_id=API_ID) 
+bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
+
+userbot = Client("saverestricted", session_string=SESSION, api_hash=API_HASH, api_id=API_ID)
 
 try:
     userbot.start()
 except BaseException:
-    print("Userbot Error ! Have you added SESSION while deploying??")
+    print("Userbot Error! Have you added SESSION while deploying??")
     sys.exit(1)
 
 Bot = Client(
     "SaveRestricted",
     bot_token=BOT_TOKEN,
-    api_id=int(API_ID),
+    api_id=API_ID,
     api_hash=API_HASH
-)    
+)
 
 try:
     Bot.start()
